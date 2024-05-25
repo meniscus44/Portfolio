7f7f902c36976d079dd2cce89e5021219e906e1f
meniscus44 <www.akshaypiyush@gmail.com>
2024-05-25 22:20:08 +0530

    Constant change

--- src/data/constants.js
+++ src/data/constants.js
@@ -338,11 +338,3 @@ export const Bio = {
     //   webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
     // },
   ];
-  
-  export const TimeLineData = [
-    { year: 2017, text: "Started my journey" },
-    { year: 2018, text: "Worked as a freelance developer" },
-    { year: 2019, text: "Founded JavaScript Mastery" },
-    { year: 2020, text: "Shared my projects with the world" },
-    { year: 2021, text: "Started my own platform" },
-  ];
